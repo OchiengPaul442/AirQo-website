bf89ea44991bae747a8b6c72e008a2c26cb399e4
Ochieng Paul <ochiengpaul442@gmail.com>
2024-10-19 12:34:03 +0300

    update

--- backend/settings.py
+++ backend/settings.py
@@ -102,14 +102,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database configuration: Separate for development (SQLite) and production (Postgres)
 if DEBUG:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
-    #         'NAME': BASE_DIR / os.getenv('DATABASE_NAME', 'db.sqlite3'),
-    #     }
-    # }
     DATABASES = {
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+        'default': {
+            'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+            'NAME': BASE_DIR / os.getenv('DATABASE_NAME', 'db.sqlite3'),
+        }
     }
 else:
     DATABASES = {
